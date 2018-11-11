561652cab0f175b0d7c15f0a4b7194fa24414d74
Unknown <taylorlemaster@my.unt.edu>
2018-11-10 22:51:49 -0600

    add asp.net Identity

--- Music/Startup.cs
+++ Music/Startup.cs
@@ -1,27 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.AspNetCore.Http.Features;
 using Microsoft.Extensions.Configuration;
+using Music.Data;
+using Microsoft.EntityFrameworkCore;
+using Music.Modals;
+using Microsoft.AspNetCore.Identity;
 
 namespace Music
 {
     public class Startup
     {
+        private string _connectionString = "Server=tcp:lemasterworks.database.windows.net,1433;Initial Catalog=MusicPlayer;Persist Security Info=False;User ID=tlemaster;Password=Lexielm2;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // extend JSON return value limit
             services.Configure<FormOptions>(x => x.ValueCountLimit = 5000);
+
+            // Identity
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(_connectionString)
+            );
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+           .AddEntityFrameworkStores<ApplicationDbContext>()
+           .AddDefaultTokenProviders();
+
+            // MVC
             services.AddMvc();
 
-            
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +58,8 @@ namespace Music
             IConfigurationRoot configurationRoot = configurationBuilder.Build();
 
             app.UseStaticFiles();
+
+            app.UseAuthentication();
 
             app.UseMvc(routes =>
             {
